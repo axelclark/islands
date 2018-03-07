ba0d5c09dabbd55e9d602c62e40380efa2fc7fbe
Axel Clark <axelclark2@yahoo.com>
2018-03-06 20:28:10 -0500

    Finish Ch 4 - Game GenServer

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -6,16 +6,26 @@ defmodule IslandsEngine.Game do
   @players [:player1, :player2]
 
   def start_link(name) when is_binary(name) do
-    GenServer.start_link(__MODULE__, name, [])
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
   end
 
   def add_player(game, name) when is_binary(name) do
     GenServer.call(game, {:add_player, name})
   end
 
+  def guess_coordinate(game, player, row, col) when player in @players do
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+  end
+
   def position_island(game, player, key, row, col) when player in @players do
     GenServer.call(game, {:position_island, player, key, row, col})
   end
+
+  def set_islands(game, player) when player in @players do
+    GenServer.call(game, {:set_islands, player})
+  end
+
+  def via_tuple(name), do: {:via, Registry, {Registry.Game, name}}
 
   # Callbacks
 
@@ -34,6 +44,32 @@ defmodule IslandsEngine.Game do
       |> reply_success(:ok)
     else
       :error -> {:reply, :error, state_data}
+    end
+  end
+
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <-
+           Rules.check(rules, {:win_check, win_status})
+    do
+      state_data
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error ->
+        {:reply, :error, state_data}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
     end
   end
 
@@ -63,6 +99,20 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:set_islands, player}, _from, state_data) do
+    board = player_board(state_data, player)
+    with {:ok, rules} <- Rules.check(state_data.rules, {:set_islands, player}),
+         true         <- Board.all_islands_positioned?(board)
+    do
+      state_data
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error -> {:reply, :error, state_data}
+      false  -> {:reply, {:error, :not_all_islands_positioned}, state_data}
+    end
+  end
+
   # Helpers
 
   # Implementations
@@ -78,4 +128,16 @@ defmodule IslandsEngine.Game do
   end
 
   defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+
+  ## :guess_coordinate
+
+  defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
+    update_in(state_data[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
+
+  defp opponent(:player1), do: :player2
+
+  defp opponent(:player2), do: :player1
 end
