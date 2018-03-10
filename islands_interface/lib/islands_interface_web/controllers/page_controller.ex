598fe8a3307c4e621bec9bf9e19f50eba380c34f
Axel Clark <axelclark2@yahoo.com>
2018-03-10 09:53:10 -0500

    Add islands_engine dep & test interface

--- islands_interface/lib/islands_interface_web/controllers/page_controller.ex
+++ islands_interface/lib/islands_interface_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule IslandsInterfaceWeb.PageController do
   use IslandsInterfaceWeb, :controller
 
+  alias IslandsEngine.GameSupervisor
+
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  def test(conn, %{"name" => name}) do
+    {:ok, _pid} = GameSupervisor.start_game(name)
+
+    conn
+    |> put_flash(:info, "You entered the name: " <> name)
+    |> render("index.html")
   end
 end
