fcc6c676c649039a6795e8cf7ed2aa88b44ebf15
Axel Clark <axelclark2@yahoo.com>
2018-03-17 18:41:40 -0400

    Add start game component

--- islands_interface/assets/src/App.vue
+++ islands_interface/assets/src/App.vue
@@ -1,14 +1,87 @@
 <template>
-  <div id="app">
-    <header>
-      <h1>Phoenix Vue</h1>
-    </header>
-  </div>
+  <h1 v-if="isGameStarted">Game Started</h1>
+  <game-start v-else v-on:startGame="startGame"/>
 </template>
 
 <script>
+import GameStart from './components/GameStart'
+import { Socket } from "phoenix";
+
 export default {
-  name: 'app'
+  name: 'app',
+
+  data () {
+    return {
+      isGameStarted: false,
+      socket: null,
+      channel: null,
+      player: null
+    }
+  },
+
+  components: {
+    GameStart
+  },
+
+  created () {
+    this.socket = new Socket("/socket", {});
+    this.socket.connect();
+    console.log("Socket connected!")
+  },
+
+  methods: {
+    startGame: function (action) {
+      const player1_channel = this.newChannel("player1");
+      const player2_channel = this.newChannel("player2");
+
+      if (action === "start-game") {
+        this.channel = player1_channel;
+        this.player = "player1";
+        this.join(player1_channel);
+        this.newGame(player1_channel);
+      } else {
+        this.channel = player2_channel;
+        this.player = "player2";
+        this.join(player2_channel);
+        this.addPlayer(player2_channel, "player2");
+      }
+      this.isGameStarted = true;
+    },
+
+    newChannel: function (screen_name) {
+      return this.socket.channel("game:player1", {screen_name: screen_name});
+    },
+
+    join: function (channel) {
+      channel.join()
+        .receive("ok", response => {
+          console.log("Joined successfully!");
+        })
+        .receive("error", response => {
+          console.log("Unable to join");
+        })
+    },
+
+    newGame: function (channel) {
+      channel.push("new_game")
+        .receive("ok", response => {
+          console.log("New Game!");
+        })
+        .receive("error", response => {
+          console.log("Unable to start a new game.");
+        })
+    },
+
+    addPlayer: function (channel, player) {
+      channel.push("add_player", player)
+        .receive("ok", response => {
+          console.log("Player added!");
+        })
+        .receive("error", response => {
+          console.log("Unable to add new player: " + player, response);
+        })
+    }
+  }
 }
 </script>
 
---
+++ islands_interface/assets/src/components/GameStart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <start-button :onClick='() => handleClick("start-game")' message='Start the Demo Game'/>
+    <start-button :onClick='() => handleClick("join-game")' message='Join the Demo Game'/>
+  </div>
+</template>
+
+<script>
+import StartButton from './StartButton'
+export default {
+  name: 'game-start',
+  components: {
+    StartButton
+  },
+  methods: {
+    handleClick: function (action) {
+      this.$emit('startGame', action)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
