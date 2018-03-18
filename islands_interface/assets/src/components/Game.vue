57b421b90dd79cf1b8ca020999eaa963ed799629
Axel Clark <axelclark2@yahoo.com>
2018-03-18 15:40:36 -0400

    Update dragstart event handler

--- islands_interface/assets/src/components/Game.vue
+++ islands_interface/assets/src/components/Game.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div id="holder">
-      <img id="atoll" src="images/atoll.png" width="60" height="90" draggable="true" v-on:ondragstart.native="dragStartHandler" />
-      <img id="dot" src="images/dot.png" width="30" height="30" draggable="true" v-on:ondragstart.native="dragStartHandler" />
-      <img id="l_shape" src="images/l_shape.png" width="60" height="90" draggable="true" v-on:ondragstart.native="dragStartHandler" />
-      <img id="s_shape" src="images/s_shape.png" width="90" height="60" draggable="true" v-on:ondragstart.native="dragStartHandler" />
-      <img id="square" src="images/square.png" width="60" height="60" draggable="true" v-on:ondragstart.native="dragStartHandler" />
+      <img id="atoll" src="images/atoll.png" width="60" height="90" draggable="true" v-on:dragstart="dragStartHandler" />
+      <img id="dot" src="images/dot.png" width="30" height="30" draggable="true" v-on:dragstart="dragStartHandler" />
+      <img id="l_shape" src="images/l_shape.png" width="60" height="90" draggable="true" v-on:dragstart="dragStartHandler" />
+      <img id="s_shape" src="images/s_shape.png" width="90" height="60" draggable="true" v-on:dragstart="dragStartHandler" />
+      <img id="square" src="images/square.png" width="60" height="60" draggable="true" v-on:dragstart="dragStartHandler" />
     </div>
     <own-board :channel="channel" :player="player" />
     <opponent-board :channel="channel" :player="player" />
@@ -28,7 +28,7 @@ export default {
   },
   methods: {
     dragStartHandler: function (event) {
-      console.log("dragging")
+      console.log("dragging", event)
       event.dataTransfer.setData("text/plain", event.target.id);
     }
   }
