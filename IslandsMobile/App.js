fd6e8d9d6dfe01c12c1e36a534064c44f7c53419
Axel Clark <axelclark2@yahoo.com>
2018-04-15 21:57:42 -0400

    Setup navigation

--- IslandsMobile/App.js
+++ IslandsMobile/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StackNavigator } from 'react-navigation'
+import StartGame from './components/StartGame'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+const App = StackNavigator({
+  StartGame: {
+    screen: StartGame
   }
-}
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
